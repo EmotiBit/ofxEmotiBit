3b1e57ae9dd054e810ba3638f6e94ed4a52b5c22
Nitin Nair <nitin7nair@gmail.com>
2020-03-19 16:21:32 -0400

    Version bump to v1.1.0

--- src/ofxEmotiBitVersion.h
+++ src/ofxEmotiBitVersion.h
@@ -1,7 +1,7 @@
 #pragma once
 //#include <string>
 #include "ofMain.h"
-const std::string ofxEmotiBitVersion = "1.0.17";
+const std::string ofxEmotiBitVersion = "1.1.0";
 
 static void writeOfxEmotiBitVersionFile() {
 	string filename = "ofxEmotiBit_Version.txt";
