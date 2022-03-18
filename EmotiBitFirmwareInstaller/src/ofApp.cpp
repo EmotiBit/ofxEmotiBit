aebf5bb365e1386170ab14cb5c4993750b3d3523
nitin <nitin7nair@gmail.com>
2022-03-18 15:54:43 -0400

    tested working for macOS'

--- EmotiBitFirmwareInstaller/src/ofApp.cpp
+++ EmotiBitFirmwareInstaller/src/ofApp.cpp
@@ -67,46 +67,42 @@ void ofApp::update(){
 	}
 	else if (_state == State::UPLOAD_WINC_FW_UPDATER_SKETCH)
 	{
-		if (tryCount <= MAX_NUM_TRIES_PING_1200)
-		{
-			// check if the upload was completed successfully
-			if (uploadWincUpdaterSketch())
-			{
-				// progress to next state;
-				progressToNextState();
-			}
-		}
-		else
+
+        // check if the upload was completed successfully
+        if (uploadWincUpdaterSketch())
+        {
+            // progress to next state;
+            progressToNextState();
+        }
+		if (tryCount > MAX_NUM_TRIES_PING_1200)
 		{
 			raiseError();
 		}
 	}
 	else if (_state == State::RUN_WINC_UPDATER)
 	{
-		if (tryCount < MAX_NUM_TRIES_PING_1200)
-		{
-			if (runWincUpdater())
-			{
-				// progress to next state;
-				progressToNextState();
-			}
-		}
-		else
+		
+        if (runWincUpdater())
+        {
+            // progress to next state;
+            progressToNextState();
+        }
+
+		if (tryCount > MAX_NUM_TRIES_PING_1200)
 		{
 			raiseError();
 		}
 	}
 	else if (_state == State::UPLOAD_EMOTIBIT_FW)
 	{
-		if (tryCount <= MAX_NUM_TRIES_PING_1200)
-		{
-			if (uploadEmotiBitFw())
-			{
-				// progress to next state;
-				progressToNextState();
-			}
-		}
-		else
+
+        if (uploadEmotiBitFw())
+        {
+            // progress to next state;
+            progressToNextState();
+        }
+
+        if (tryCount > MAX_NUM_TRIES_PING_1200)
 		{
 			raiseError();
 		}
@@ -142,7 +138,7 @@ void ofApp::raiseError(std::string additionalMessage)
 {
 	onScreenInstruction = onScreenInstructionList[State::INSTALLER_ERROR];
 	// set the Error string according to the current state
-	displayedErrorMessage = errorMessageList[_state] + additionalMessage;
+	displayedErrorMessage = additionalMessage + errorMessageList[_state];
 	_state = State::INSTALLER_ERROR;
 	tryCount = 0;
 }
@@ -310,7 +306,14 @@ std::vector<std::string> ofApp::getComPortList(bool printOnConsole)
 	// convert device list into COM ports
 	for (int i = 0; i < initDeviceList.size(); i++)
 	{
-		comPortList.push_back(initDeviceList.at(i).getDevicePath());
+#ifdef TARGET_OSX
+        if(initDeviceList.at(i).getDevicePath().find("/dev/tty") != std::string::npos)
+        {
+            comPortList.push_back(initDeviceList.at(i).getDevicePath());
+        }
+#else
+        comPortList.push_back(initDeviceList.at(i).getDevicePath());
+#endif
 	}
 	// sort the list
 	std::sort(comPortList.begin(), comPortList.end());
@@ -330,24 +333,26 @@ std::vector<std::string> ofApp::getComPortList(bool printOnConsole)
 
 bool ofApp::initProgrammerMode(std::string &programmerPort)
 {
+    tryCount++;
 	if (tryCount < MAX_NUM_TRIES_PING_1200)
 	{
-		tryCount++;
 		ofLog(OF_LOG_NOTICE, "Ping try: " + ofToString(tryCount));
 #if defined(TARGET_LINUX) || defined(TARGET_OSX)
 		// set to bootloader mode
-		// sonnect to serial port
-		// ToDo: create a screen session with a specific name using -S. This way we can kill that specific session
+		// connect to serial port
 		ofLog(OF_LOG_NOTICE, "connecting using screen");
-		std::string command = "screen -d -m " + featherPort + " 1200";
+		std::string command = "screen -d -m -S featherM0 " + featherPort + " 1200";
 		system(command.c_str());
 		// dicsonnect serial port
 		ofSleepMillis(1000);
 		ofLog(OF_LOG_NOTICE, "disconneting");
-		system("screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill");
-		command.clear();
-		programmerPort = featherPort;
-		return true;
+        command.clear();
+        command = "screen -XS featherM0 quit";
+        ofSleepMillis(500);
+		//system("screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill");
+        system(command.c_str());
+        
+		//programmerPort = featherPort;
 #else
 		ofSerial serial;
 		// get initial list of COM ports
@@ -368,6 +373,7 @@ bool ofApp::initProgrammerMode(std::string &programmerPort)
 			comListWithProgrammingPort = updatedComPortList;
 			return true;
 		}
+#endif
 	}
 	else
 	{
@@ -379,7 +385,6 @@ bool ofApp::initProgrammerMode(std::string &programmerPort)
 		return true;
 	}
 	return false;
-#endif
 }
 
 std::string ofApp::findNewComPort(std::vector<std::string> oldList, std::vector<std::string> newList)
@@ -468,21 +473,7 @@ bool ofApp::updateUsingBossa(std::string filePath)
 bool ofApp::uploadWincUpdaterSketch()
 {
 #if defined(TARGET_LINUX) || defined(TARGET_OSX)
-    //std::string dontCarePort;
-    // set feather in programmer mode
-    //initProgrammerMode(dontCarePort);
-    //ofLog(OF_LOG_NOTICE,"waiting to flash with bossa");
-    //ofSleepMillis(5000);
-    //ofLog(OF_LOG_NOTICE, "uploading firmware updater sketch");
-    // upload the firmware updater sketch
-    //std::string command;
-    //system("pwd");
-    //system("ls");
-    //command = "../MacOS/bossac -i -d -U true -e -w -v -R -b -p " + featherPort + " WINC/FirmwareUpdater.ino.feather_m0.bin";
-    //ofLogNotice("BOSSA Command") << command;
-    //system(command.c_str());
     return updateUsingBossa("WINC/FirmwareUpdater.ino.feather_m0.bin");
-    //return true;
 #else
 	return updateUsingBossa("data\\WINC\\FirmwareUpdater.ino.feather_m0.bin");
 #endif
@@ -491,13 +482,6 @@ bool ofApp::uploadWincUpdaterSketch()
 bool ofApp::runWincUpdater()
 {
 #if defined(TARGET_LINUX) || defined(TARGET_OSX)
-    // run the WINC updater
-    //ofLog(OF_LOG_NOTICE, "Updating Winc FW using WiFi101 updater");
-    //ofSleepMillis(3000);
-    //std::string command = std::string("../MacOS/FirmwareUploader -firmware WINC/m2m_aio_3a0.bin ") + std::string("-port ") + featherPort;
-    //ofLogNotice("BOSSA Command") << command;
-    //system(command.c_str());
-    //return true;
     if (!systemCommandExecuted)
     {
         ofLog(OF_LOG_NOTICE, "Updating Winc FW using WiFi101 updater");
@@ -507,20 +491,6 @@ bool ofApp::runWincUpdater()
         threadedSystemCall.setup(command);
         threadedSystemCall.startThread();
         systemCommandExecuted = true;
-        // get updated COM list. the feather returns back to feather port, after the bossa flash is completed.
-        //std::vector<std::string> newComPortList = getComPortList(true);
-        // find the feather port
-        //featherPort = findNewComPort(comListWithProgrammingPort, newComPortList); // old list, new list
-//
-//        if (featherPort.compare(COM_PORT_NONE) != 0)
-//        {
-//            ofLog(OF_LOG_NOTICE, "Feather found at: " + featherPort);
-//            std::string command = "data\\WINC\\FirmwareUploader.exe -port " + featherPort + " -firmware " + "data\\WINC\\m2m_aio_3a0.bin";
-//            ofLogNotice("UPDATING WINC FW: COMMAND: ") << command;
-//            threadedSystemCall.setup(command);
-//            threadedSystemCall.startThread();
-//            systemCommandExecuted = true;
-//        }
     }
     else
     {
@@ -556,15 +526,6 @@ bool ofApp::runWincUpdater()
 bool ofApp::uploadEmotiBitFw()
 {
 #if defined(TARGET_LINUX) || defined(TARGET_OSX)
-    ///std::string dontCarePort;
-    //initProgrammerMode(dontCarePort);
-    //ofLog(OF_LOG_NOTICE,"waiting to flash with bossa");
-    //ofSleepMillis(5000);
-    // upload the firmware updater sketch
-    //std::string command = "../MacOS/bossac -i -d -U true -e -w -v -R -b -p " + featherPort + " EmotiBit_stock_firmware.ino.feather_m0.bin";
-    //ofLogNotice("BOSSA Command") << command;
-    //system(command.c_str());
-    //return true;
     return updateUsingBossa("EmotiBit_stock_firmware.ino.feather_m0.bin");
 #else
 	return updateUsingBossa("data\\EmotiBit_stock_firmware.ino.feather_m0.bin");
