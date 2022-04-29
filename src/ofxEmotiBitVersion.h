6b389b1c99803e77fc10c2da9246eace7458c724
Nitin Nair <nitin7nair@gmail.com>
2022-04-29 17:08:01 -0400

    version bump to v1.4.3

--- src/ofxEmotiBitVersion.h
+++ src/ofxEmotiBitVersion.h
@@ -1,7 +1,7 @@
 #pragma once
 //#include <string>
 #include "ofMain.h"
-const std::string ofxEmotiBitVersion = "1.4.1.fix-dataParser.9";
+const std::string ofxEmotiBitVersion = "1.4.3";
 
 static void writeOfxEmotiBitVersionFile() {
 	string filename = "ofxEmotiBit_Version.txt";
