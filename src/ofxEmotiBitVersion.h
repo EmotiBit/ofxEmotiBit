59829193dadadc767ceaa7af1e1141c9981eca1e
Nitin Nair <nitin7nair@gmail.com>
2022-05-02 11:21:22 -0400

    changed column header from EpochTimestamp to LocalTimestamp in parsed files

--- src/ofxEmotiBitVersion.h
+++ src/ofxEmotiBitVersion.h
@@ -1,7 +1,7 @@
 #pragma once
 //#include <string>
 #include "ofMain.h"
-const std::string ofxEmotiBitVersion = "1.4.3";
+const std::string ofxEmotiBitVersion = "1.4.4";
 
 static void writeOfxEmotiBitVersionFile() {
 	string filename = "ofxEmotiBit_Version.txt";
