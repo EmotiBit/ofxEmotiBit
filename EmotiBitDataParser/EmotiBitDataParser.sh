1b943140c16e05f42c84b6bda606eab49e10dd0e
Sean Montgomery <produceconsumerobot@gmail.com>
2022-05-12 09:16:45 -0700

    cd dataDir

---
+++ EmotiBitDataParser/EmotiBitDataParser.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+exePath=$(pwd)
+dataDir=$(pwd)
+getHelp=false
+
+echo -e "";
+echo -e "** EmotiBitDataParser.sh ** ";
+echo -e "Runs EmotiBitDataParser on *.csv in passed data directory\n";
+echo -e 'Example input:\n./EmotiBitDataParser.sh -x "C:\\Program Files\\EmotiBit\\EmotiBit DataParser\\EmotiBitDataParser.exe" -d "C:\\priv\\local\\EmotiBitData\\"\n'
+
+while getopts x:d: flag
+do
+    case "${flag}" in
+        x) exePath=${OPTARG};;
+        d) dataDir=${OPTARG};;
+    esac
+done
+
+# Replace backslashes with forward slashes
+exePath="${exePath//\\//}"
+dataDir="${dataDir//\\//}"
+
+echo "[-x] exePath: $exePath";
+echo "[-d] dataDir: $dataDir";
+
+currDir=$(pwd);
+echo -e "\npwd: $currDir"
+
+cd "$dataDir"
+
+for file in $(eval "ls *.csv")
+do
+	echo -e "\nprocessing: $file"
+	subDirName=$(basename $file .csv)
+	echo "mkdir: $subDirName"
+	mkdir $subDirName
+  mv $file $subDirName
+	mv "$subDirName"_info.json $subDirName
+	cd "$currDir" #cd to currDir before cmd Prevents ofDirectory errors
+	cmd="\"$exePath\" \"$dataDir/$subDirName/$subDirName.csv\""
+	echo "cmd: $cmd"
+	eval $cmd
+	cd "$dataDir"
+done
+
+cd "$currDir"
