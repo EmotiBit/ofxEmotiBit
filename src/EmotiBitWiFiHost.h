3ee4242fd7ed9214d85a28c6697584b0434d55cf
Sean Montgomery <produceconsumerobot@gmail.com>
2022-05-12 09:13:16 -0700

    Added documentation

--- src/EmotiBitWiFiHost.h
+++ src/EmotiBitWiFiHost.h
@@ -72,7 +72,7 @@ public:
 	string connectedEmotibitIp;
 	bool _isConnected;
 	bool isStartingConnection;
-	uint16_t startCxnTimeout = 5000;
+	uint16_t startCxnTimeout = 5000;	// milliseconds
 	uint64_t startCxnAbortTimer;
 
 	uint16_t advertisingPacketCounter = 0;
@@ -115,14 +115,43 @@ public:
 	//string createPacket(string typeTag, vector<string> data, uint8_t protocolVersion = 1, uint8_t dataReliability = 100);
 	bool isConnected();
 	int8_t _startDataCxn(uint16_t dataPort);
+
+	/*!
+			@brief returns whether the passed ipAddress is in the network includeList
+			@param ipAddress
+			@return true if ipAddress is in the includeList
+	*/
 	bool isInNetworkIncludeList(string ipAddress);
+
+	/*!
+		@brief returns whether the passed ipAddress is in the network excludeList
+		@param ipAddress
+		@return true if ipAddress is in the excludeList
+	*/
 	bool isInNetworkExcludeList(string ipAddress);
+
+	/*!
+	@brief returns whether the passed ipAddress is in passed network list
+	@param ipAddress to check
+	@param networkList of ipAddresses to check ipAddress against
+	@return true if ipAddress is in networkList
+	*/
 	bool isInNetworkList(string ipAddress, vector<string> networkList);
 
 	void setAdvertTransSettings(bool enableBroadcast, bool enableUnicast, pair<int, int> unicastIpRange);
 	void setNetworkIncludeList(vector<string> networkIncludeList);
 	void setNetworkExcludeList(vector<string> networkExcludeList);
+
+	/*!
+	@brief Sets EmotiBit host advertising settings
+	@param settings
+	*/
 	void setHostAdvertisingSettings(HostAdvertisingSettings settings);
+
+	/*!
+	@brief Gets EmotiBit host advertising settings
+	@return settings
+	*/
 	HostAdvertisingSettings getHostAdvertisingSettings();
 
 	string ofGetTimestampString(const string& timestampFormat); // Adds %f for microseconds
