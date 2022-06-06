ea104f6e6e1a6e1445bbcb4dc3758709d45371d8
Nitin Nair <nitin7nair@gmail.com>
2022-06-06 14:43:50 -0400

    version bump to 1.5.0

--- src/ofxEmotiBitVersion.h
+++ src/ofxEmotiBitVersion.h
@@ -2,7 +2,7 @@
 //#include <string>
 #include "ofMain.h"
 
-const std::string ofxEmotiBitVersion = "1.4.11.feat-lslParsing.9";
+const std::string ofxEmotiBitVersion = "1.5.0";
 static const char SOFTWARE_VERSION_PREFIX = 'v';
 
 static void writeOfxEmotiBitVersionFile() {
