e506ac805bfcf383138152968cbd212f33b9fc05
Nitin Nair <nitin7nair@gmail.com>
2022-07-29 10:34:56 -0400

    added new FW bins 1.4.1. Version bump

--- src/ofxEmotiBitVersion.h
+++ src/ofxEmotiBitVersion.h
@@ -2,7 +2,7 @@
 //#include <string>
 #include "ofMain.h"
 
-const std::string ofxEmotiBitVersion = "1.5.8";
+const std::string ofxEmotiBitVersion = "1.5.9";
 static const char SOFTWARE_VERSION_PREFIX = 'v';
 
 static void writeOfxEmotiBitVersionFile() {
