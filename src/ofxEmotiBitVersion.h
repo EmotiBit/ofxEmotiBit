2e08c16763896a91ac7322ed2d3408a34bcb3668
Nitin Nair <nitin7nair@gmail.com>
2022-07-29 10:45:16 -0400

    fixed dataparser icon. SW version bump

--- src/ofxEmotiBitVersion.h
+++ src/ofxEmotiBitVersion.h
@@ -2,7 +2,7 @@
 //#include <string>
 #include "ofMain.h"
 
-const std::string ofxEmotiBitVersion = "1.5.9";
+const std::string ofxEmotiBitVersion = "1.5.10";
 static const char SOFTWARE_VERSION_PREFIX = 'v';
 
 static void writeOfxEmotiBitVersionFile() {
