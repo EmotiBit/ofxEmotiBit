5016c9d0fc09c883afe8202740ffc4394ef5fb86
Sean Montgomery <produceconsumerobot@gmail.com>
2022-11-12 03:36:08 -0800

    bumped version

--- src/ofxEmotiBitVersion.h
+++ src/ofxEmotiBitVersion.h
@@ -2,7 +2,7 @@
 //#include <string>
 #include "ofMain.h"
 
-const std::string ofxEmotiBitVersion = "1.5.10";
+const std::string ofxEmotiBitVersion = "1.5.10.feat-specifyFwFile.0";
 static const char SOFTWARE_VERSION_PREFIX = 'v';
 
 static void writeOfxEmotiBitVersionFile() {
