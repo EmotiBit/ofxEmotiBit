23c235be1710cf6d1b38f057705abac53d82f3dd
Sean Montgomery <produceconsumerobot@gmail.com>
2024-03-22 17:28:11 -0700

    Bumped version

--- src/ofxEmotiBitVersion.h
+++ src/ofxEmotiBitVersion.h
@@ -3,7 +3,7 @@
 #include "ofMain.h"
 
 
-const std::string ofxEmotiBitVersion = "1.9.0-alpha";
+const std::string ofxEmotiBitVersion = "1.9.0-alpha.feat-LslOutput.0";
 
 static const char SOFTWARE_VERSION_PREFIX = 'v';
 
