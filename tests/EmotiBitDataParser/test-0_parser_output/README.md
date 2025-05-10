fbc122d622c6fc21fae0311a62541954031e750b
Nitin Nair <nitin7nair@gmail.com>
2025-05-10 12:19:46 -0400

    updated testeing dir

--- tests/EmotiBitDataParser/test-0_parser_output/README.md
+++ tests/EmotiBitDataParser/test-0_parser_output/README.md
@@ -1,3 +1,2 @@
 # Description
-- This tests verifies that the DataParser creates expected output for the sample data.
-- The parsed data will be verified with an MD5 sum.
+- Run the parser on the sample data
---
+++ tests/EmotiBitDataParser/test-0_parser_output/run_test.sh
@@ -0,0 +1,8 @@
+echo "Parsing sample data"
+${ vars.OFXEMOTIBIT_DIR }/EmotiBitDataParser/bin/EmotiBitDataParser "${ vars.OFXEMOTIBIT_DIR }/tests/EmotiBitDataParser/sample_data/2025-03-20_12-09-40-822726.csv"
+if [ $? -eq 0 ]; then
+  echo "Sample file parsed successfully"
+else
+  echo "parsing failed"
+  exit 1
+fi
