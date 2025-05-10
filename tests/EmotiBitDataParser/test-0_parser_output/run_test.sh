ea98b26928ff176945adb59e8882ccc57e6cb44d
Nitin Nair <nitin7nair@gmail.com>
2025-05-10 12:58:49 -0400

    updated yaml

--- tests/EmotiBitDataParser/test-0_parser_output/run_test.sh
+++ tests/EmotiBitDataParser/test-0_parser_output/run_test.sh
@@ -1,5 +1,5 @@
 echo "Parsing sample data"
-$OFXEMOTIBIT_DIR/EmotiBitDataParser/bin/EmotiBitDataParser "$OFXEMOTIBIT_DIR/tests/EmotiBitDataParser/sample_data/2025-03-20_12-09-40-822726.csv"
+$ENV_OFXEMOTIBIT_DIR/EmotiBitDataParser/bin/EmotiBitDataParser "$ENV_OFXEMOTIBIT_DIR/tests/EmotiBitDataParser/sample_data/2025-03-20_12-09-40-822726.csv"
 if [ $? -eq 0 ]; then
   echo "Sample file parsed successfully"
 else
