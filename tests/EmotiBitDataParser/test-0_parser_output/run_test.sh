f197f06b50152933e79a0d2cdb397cd808b22986
Nitin Nair <nitin7nair@gmail.com>
2025-06-14 01:44:51 -0400

    corrected the run_test for test_0

--- tests/EmotiBitDataParser/test-0_parser_output/run_test.sh
+++ tests/EmotiBitDataParser/test-0_parser_output/run_test.sh
@@ -1,8 +1,34 @@
 echo "Parsing sample data"
-${ENV_OFXEMOTIBIT_DIR}/EmotiBitDataParser/bin/EmotiBitDataParser "${ENV_OFXEMOTIBIT_DIR}/tests/EmotiBitDataParser/sample_data/2025-03-20_12-09-40-822726.csv"
-if [ $? -eq 0 ]; then
-  echo "Sample file parsed successfully"
-else
-  echo "parsing failed"
+unset ENV_OFXEMOTIBIT_DIR
+
+OPTIONS=$(getopt -o d: --long --ofxemotibit-dir: -n "run_tesh.sh" -- "$@")
+
+if [ $? -ne 0 ]; then
+  echo "Terminating script." >&2
   exit 1
 fi
+
+eval set -- "$OPTIONS"
+
+while true; do
+  case "$1" in
+    -d | --ofxemotibit-dir)
+      echo "ofxEmotibit dir location"
+      ENV_OFXEMOTIBIT_DIR=$2
+      shift 2
+      ;;
+    --) # End of options marker
+      shift
+      break
+      ;;
+  esac
+done
+
+if [ -z "$ENV_OFXEMOTIBIT_DIR" ]; then
+	echo "path to ofxEmotiBit not provided. Aborting"
+	exit 1
+fi
+
+
+${ENV_OFXEMOTIBIT_DIR}/EmotiBitDataParser/bin/EmotiBitDataParser "${ENV_OFXEMOTIBIT_DIR}/tests/EmotiBitDataParser/sample_data/2025-03-20_12-09-40-822726.csv"
+
