437dfcf087250c96d08aa789f39a66bf12f7ea4c
Nitin Nair <nitin7nair@gmail.com>
2025-06-24 17:31:25 -0400

    fixed find_unused_typetags test

--- tests/EmotiBitDataParser/find_unused_typetags/run_test.sh
+++ tests/EmotiBitDataParser/find_unused_typetags/run_test.sh
@@ -34,25 +34,25 @@ fi
 
 ${ENV_OFXEMOTIBIT_DIR}/EmotiBitDataParser/bin/EmotiBitDataParser "${ENV_OFXEMOTIBIT_DIR}/tests/EmotiBitDataParser/sample_data/2025-03-20_12-09-40-822726.csv"
 
+cd ${ENV_OFXEMOTIBIT_DIR}/tests/EmotiBitDataParser/find_unused_typetags
 echo "find missing typetags from parsed data"
 emotibitPacketFile="temp_emotibitpacket.txt"
 curl https://raw.githubusercontent.com/EmotiBit/EmotiBit_XPlat_Utils/refs/heads/master/src/EmotiBitPacket.cpp > $emotibitPacketFile
 tt_list=""
-
 directory="../sample_data"
 basename="2025-03-20_12-09-40-822726"
 for file in "$directory"/*; do
     if [ -f "$file" ]; then
-		file=${file#$directory"/"}
-		if echo $file | grep -q ".csv"; then  # if its a csv file
-			dot_index=`expr index "$file" .`  # find index of "."
-			ext=${file:dot_index:3}  # extract extension
-			tt=${file#$basename"_"}  # extract typetag
-			tt=${tt%"."$ext}  # extract typetag
-			file_tt_list="${file_tt_list} ${tt}" # appenf typetag to list
-			#echo $tt
-			#echo "$file"
-		fi
+	file=${file#$directory"/"}
+	if echo $file | grep -q ".csv"; then  # if its a csv file
+		dot_index=`expr index "$file" .`  # find index of "."
+		ext=${file:$dot_index:3}  # extract extension
+		tt=${file#$basename"_"}  # extract typetag
+		tt=${tt%"."$ext}  # extract typetag
+		file_tt_list="${file_tt_list} ${tt}" # append typetag to list
+		#echo $tt
+		#echo "$file"
+	fi
     fi
 done
 tt_ignore_list="ER O2 T0 H0 BS BL SD RS TE TU EI GL GS GB GA RE S+ S- PN PO HE HH EC ED WA WD LS MN ML MM MO MH"
@@ -64,7 +64,7 @@ while IFS= read -r line; do
 		continue
 	else
 		tt_index=`expr index "$line" =` # find typetag definition index
-		tt=${line:tt_index+2:2}  # extract typetag
+		tt=${line:$tt_index+2:2}  # extract typetag
 		if echo "$file_tt_list" | grep -q "$tt"; then
 			continue
 		else
