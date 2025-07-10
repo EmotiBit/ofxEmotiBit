9b35e509dc4e2c4102655a2779628e36b7a51d74
Nitin Nair <nitin7nair@gmail.com>
2025-07-10 10:57:27 -0400

    removing legacy section from bash script

--- tests/EmotiBitInstaller/check-installer-version/run_test.sh
+++ tests/EmotiBitInstaller/check-installer-version/run_test.sh
@@ -1,35 +1,4 @@
 #!/bin/bash
-
-#echo "Parsing sample data"
-#unset ENV_OFXEMOTIBIT_DIR
-#
-#OPTIONS=$(getopt -o d: --long --ofxemotibit-dir: -n "run_tesh.sh" -- "$@")
-#
-#if [ $? -ne 0 ]; then
-  #echo "Terminating script." >&2
-  #exit 1
-#fi
-#
-#eval set -- "$OPTIONS"
-#
-#while true; do
-  #case "$1" in
-    #-d | --ofxemotibit-dir)
-      #echo "ofxEmotibit dir location: $2"
-      #ENV_OFXEMOTIBIT_DIR=$2
-      #shift 2
-      #;;
-    #--) # End of options marker
-      #shift
-      #break
-      #;;
-  #esac
-#done
-#
-#if [ -z "$ENV_OFXEMOTIBIT_DIR" ]; then
-	#echo "path to ofxEmotiBit not provided. Aborting"
-	#exit 1
-#fi
 product_version_str=`grep ProductVersion ../../../EmotiBitInstaller/EmotiBitInstaller/EmotiBitInstaller.vdproj`
 #echo $product_version_str
 productVersion=$(echo "$product_version_str" | sed 's/.*8://' | sed 's/"//')
