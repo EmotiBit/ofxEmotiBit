8f6cc2580e252ad8023791c8d10b8e40a1a71ca3
Nitin Nair <nitin7nair@gmail.com>
2026-03-30 22:57:37 -0400

    changing background to white

--- src/ofxEmotiBitVersion.h
+++ src/ofxEmotiBitVersion.h
@@ -2,15 +2,15 @@
 //#include <string>
 #include "ofMain.h"
 
-const std::string ofxEmotiBitVersion = "1.15.0";
+const std::string ofxEmotiBitVersion = "1.16.0";
 
 static const char SOFTWARE_VERSION_PREFIX = 'v';
 
 static void writeOfxEmotiBitVersionFile() {
 	string filename = "ofxEmotiBit_Version.txt";
 	remove(ofToDataPath(filename).c_str());
-	ofstream mFile;
-	mFile.open(ofToDataPath(filename).c_str(), ios::out);
+	std::ofstream mFile;
+	mFile.open(ofToDataPath(filename).c_str(), std::ios::out);
 	mFile << ofxEmotiBitVersion.c_str();
 	mFile.close();
 }
